3db81dba22920b995d27832e7bd44d314d8296aa
kbb306 <kylerbbelshaw@gmail.com>
2025-09-18 20:05:29 -0600

    Added the colors you asked for and created attending1.js

---
+++ SUU BS/scripts/attending1.js
@@ -0,0 +1,2 @@
+document.getElementById("date").min = new Date().toISOString().split("T")[0];
+// Found this here: https://stackoverflow.com/questions/74585566/set-date-input-fields-minimum-date-to-today
