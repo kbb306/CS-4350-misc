a4e496395d26b0d7cdf650400d3e39c1ed474b19
= <=>
2025-09-24 14:24:39 -0600

    Too many changes to count

--- SUU BS/scripts/attending2.js
+++ SUU BS/scripts/attending2.js
@@ -5,6 +5,7 @@ for (var i = 0; i<Hat.length; i++) {
     var option = document.createElement("option");
     option.textContent = House;
     option.value = House;
+    option.onclick= "addpoints(5)";
     Hogwarts.appendChild(option);
 }
 
@@ -15,7 +16,51 @@ var characters = [ "Harry Potter",
         "Luna Lovegood",
         "Draco Malfoy",
         "A character not listed"]
+
+var formsection = document.getElementById("chars")
 for (var i = 0; i < characters.length; i++) {
     who = characters[i];
+    var newbutton = document.createElement("input");
+    newbutton.type = "radio"
+    newbutton.textContent = who;
+    newbutton.name = who
+    newbutton.id = who
+    newbutton.value = who;
+    newbutton.onclick= "addpoints(10)";
+    formsection.append(newbutton);
     
+}
+
+function getFirstForm() {
+    var basics = window.location.search
+    const basicData = new URLSearchParams(basics);
+    fname = basicData.get("fname");
+    lname = basicData.get("lname");
+    movein = basicData.get("date");
+    major = basicData.get("major");
+    return [fname,lname,movein,major];
+}
+
+results = getFirstForm()
+var fname = document.createElement("input");
+fname.setAttribute("type","hidden");
+fname.setAttribute("value",results[0]);
+fname.setAttribute("name","fname");
+
+var lname = document.createElement("input");
+lname.setAttribute("type","hidden");
+lname.setAttribute("value",results[1]);
+lname.setAttribute("name","lname");
+
+var date = document.createElement("date");
+date.setAttribute("type","hidden");
+date.setAttribute("value",results[2]);
+date.setAttribute("name","date");
+
+var major = document.createElement("input");
+major.setAttribute("type","hidden");
+major.setAttribute("value",results[3]);
+
+function addpoints(points) {
+
 }
