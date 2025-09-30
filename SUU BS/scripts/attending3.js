cb7f5f6a9eda22f0f62e633b32afc88f4b577abf
kbb306 <kylerbbelshaw@gmail.com>
2025-09-29 20:58:17 -0600

    Prototype getPrevForms() function.

--- SUU BS/scripts/attending3.js
+++ SUU BS/scripts/attending3.js
@@ -2,7 +2,12 @@ function getPrevForms() {
     var prevs = window.location.search
     const prevdata = new URLSearchParams(prevs)
     const myForm = prevdata.forms[0]
+    current = document.getElementById("evenmore")
     for (var i = 0; i<myForm.length; i++) {
         const thing = myForm[i]
-        }
+        x = document.createElement("input")
+        x.setAttribute("type","hidden")}
+        x.setAttribute("value",thing.value)
+        current.append(x)
+
 }
