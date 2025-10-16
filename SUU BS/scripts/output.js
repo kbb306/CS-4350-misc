53a2b2edc3b57dc0e9bd621689b58925aaa8e357
kbb306 <kylerbbelshaw@gmail.com>
2025-10-15 22:18:25 -0600

    Added special logic for special values.

--- SUU BS/scripts/output.js
+++ SUU BS/scripts/output.js
@@ -1,7 +1,14 @@
 function dataDump() {
     var previous = window.location.search
+    var fullname = []
     var toParse = new URLSearchParams(previous)
     for (var [key,value] of toParse) {
+        if (key.search(RegExp("/.name/"))) {
+            fullname.push([key,value])
+        }
+        else if (key == "programmer") {
+            console.log("programmed by "+ value)
+        }
         place = document.getElementById(String(key))
         console.log(place.id,key,value)
         prefix = place.innerHTML
