e2b11757d1cf7b8341b328caf0619c0c58019524
kbb306 <kylerbbelshaw@gmail.com>
2025-10-15 21:46:55 -0600

    Created value placement loop.

--- SUU BS/scripts/output.js
+++ SUU BS/scripts/output.js
@@ -3,6 +3,9 @@ function dataDump() {
     var toParse = new URLSearchParams(previous)
     for (var [key,value] of toParse) {
         console.log(key)
+        place = document.getElementById(key)
+        prefix = place.innerHTML
+        place.innerHTML = prefix + value
     }
 }
 dataDump()
