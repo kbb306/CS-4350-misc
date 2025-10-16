6c4979b19eaad965d077a2cd40e9aaeb2840ef7b
= <=>
2025-10-15 18:41:47 -0600

    Added House to form.

--- SUU BS/scripts/output.js
+++ SUU BS/scripts/output.js
@@ -2,7 +2,7 @@ function dataDump() {
     var previous = window.location.search
     var toParse = new URLSearchParams(previous)
     for (var [key,value] of toParse) {
-        console.log(key)
+        
     }
 }
 dataDump()
