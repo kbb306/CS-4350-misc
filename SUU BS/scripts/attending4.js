c64f72b57dd3b6e17d9c01d1bbecc6c0a47554f2
= <=>
2025-10-15 18:21:46 -0600

    Added hash support

--- SUU BS/scripts/attending4.js
+++ SUU BS/scripts/attending4.js
@@ -1,6 +1,21 @@
 OK = false
 already = false
 
+const cyrb53 = (str, seed = 0) => { // Taken from https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 function getPrevForms() {
     var prevs = window.location.search
     const prevdata = new URLSearchParams(prevs)
@@ -62,6 +77,15 @@ function validate(form) {
     
     }
     OK = passwordcheck()
+    if (OK) {
+        password = document.getElementById("password")
+        random = Math.random()*Document.getElementById("form4").length
+        hash = document.createElement("input")
+        hash.setAttribute("type","hidden")
+        hash.setAttribute("name","password")
+        hash.setAttribute("value",cyrb53(password,random))
+
+    }
     return OK
 }
 getPrevForms()
