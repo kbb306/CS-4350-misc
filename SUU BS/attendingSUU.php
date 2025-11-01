080de20b817db87d1a520c9e9444ac90d8840e81
kbb306 <kylerbbelshaw@gmail.com>
2025-10-31 18:55:56 -0600

    Added code to handle select menus.

--- SUU BS/attendingSUU.php
+++ SUU BS/attendingSUU.php
@@ -1,9 +1,10 @@
 
-<?php void function get_form($num) {
+<?php 
+function get_form($num) {
     $servername = "localhost";
     $username = "root";
     $password = "Legally18";
-    $dbname = "userdata";
+    $dbname = "suubs";
     $conn = new mysqli($servername, $username,$password,$dbname)
     if ($conn->connect_error) {
         die("Connection failed:".$conn->connect_error);
@@ -15,7 +16,21 @@
             $var = $row["variable_name"]
             $text = $row["text_for_display"]
             $type = $row["html_input_selector"]
-            
+            print "<form name=page_$num id=page_$num>\n"
+            print "<label for=$var>$text<input type=$type>"
+            if ($type == "select") (
+                $sql = "SELECT * FROM info_for_select_and_radio_input WHERE selector=$var ORDER BY option"
+                $options = $conn->query($sql)
+                print "<select>\n"
+                while($entry = $options->fetch_assoc()) {
+                    $option = entry["option"]
+                    print "\t <option value=$option name=$var>$option</option>\n"
+                }
+                print("</select>")
+
+
+            )
+
         }
     }
 }
