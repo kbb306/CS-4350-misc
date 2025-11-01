65d54d69d5aa0b01fa995796057d2e9b659edfa1
kbb306 <kylerbbelshaw@gmail.com>
2025-10-31 18:26:44 -0600

    Started transition to PHP

---
+++ SUU BS/attendingSUU.php
@@ -0,0 +1,21 @@
+
+void function get_form($num) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "Legally18";
+    $dbname = "userdata";
+    $conn = new mysqli($servername, $username,$password,$dbname)
+    if ($conn->connect_error) {
+        die("Connection failed:".$conn->connect_error);
+    }
+    $sql = "SELECT * FROM user_inputs for_all_pages WHERE page=$num ORDER BY page_order";
+    $result = $conn->query($sql)
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()){
+            $var = $row["variable_name"]
+            $text = $row["text_for_display"]
+            
+        }
+    }
+}
+
