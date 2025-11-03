1a524926d102ba55a69a7fe6a0d46019596d4f15
kbb306 <kylerbbelshaw@gmail.com>
2025-11-03 13:27:16 -0700

    Fixed quote errors.

--- SUU BS/attendingSUU.php
+++ SUU BS/attendingSUU.php
@@ -9,7 +9,7 @@ function get_form($num) {
     if ($conn->connect_error) {
         die("Connection failed:".$conn->connect_error);
     }
-     print"<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     print"<html lang='en'><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
     
         <title>Attending SUU</title>";
     css_import(num);
@@ -29,8 +29,8 @@ function get_form($num) {
                 $options = $conn->query($sql);
                 print "\t<select>\n";
                 while($entry = $options->fetch_assoc()) {
-                    $option = entry["option"];
-                    $selector = entry["selector"];
+                    $option = $entry["option"];
+                    $selector = $entry["selector"];
                     print "\t\t<option value=$option name=$selector>$option</option>\n";
                 }
                 print("\t</select>\n");
@@ -39,16 +39,16 @@ function get_form($num) {
                 $sql = "SELECT * FROM info_for_select_and_radio_input WHERE selector=$var ORDER BY option"
                 $options = $conn->query($sql);
                 while($entry = $options->fetch_assoc()) {
-                    $option = entry["option"];
-                    $selector = entry["selector"];
+                    $option = $entry["option"];
+                    $selector = $entry["selector"];
                     print "\t<label for=$selector>$option</label>\n";
                     print "\t<input type=$type name=$selector value=$option>";
                 }
             }
 
         }
-        print "\t<input type="reset" value="Reset" id="reset">\n";
-        print "\t<input type="submit" value="Submit" id="submit" onclick="get_form($num+1)>\n"";
+        print "\t<input type='reset' value='Reset' id='reset'>\n";
+        print "\t<input type='submit' value='Submit' id='submit' onclick='get_form($num+1)>\n'";
         print "</form>";
     }
 }
