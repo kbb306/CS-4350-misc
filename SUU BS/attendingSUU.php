3ef65d61c589513d9b0497c3b94ee10dd9acb7d3
kbb306 <kylerbbelshaw@gmail.com>
2025-11-03 13:14:18 -0700

    More semicolons

--- SUU BS/attendingSUU.php
+++ SUU BS/attendingSUU.php
@@ -5,7 +5,7 @@ function get_form($num) {
     $username = "root";
     $password = "Legally18";
     $dbname = "suubs";
-    $conn = new mysqli($servername, $username,$password,$dbname)
+    $conn = new mysqli($servername, $username,$password,$dbname);
     if ($conn->connect_error) {
         die("Connection failed:".$conn->connect_error);
     }
