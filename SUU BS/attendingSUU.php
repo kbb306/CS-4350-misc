45c698a152b2abc9a2a8fe9c56204f4c47e6945b
kbb306 <kylerbbelshaw@gmail.com>
2025-11-03 18:43:51 -0700

    Added default value to date

--- SUU BS/attendingSUU.php
+++ SUU BS/attendingSUU.php
@@ -31,7 +31,7 @@ function get_form($num=0) {
                 print "\t<label for=$var>$text<input type=$type name=$var id=$var>\n";
             }
             else if ($type == "date") {
-                print "\t<label for=$var>$text<input type=$type name=$var id=$var min=$date>\n";
+                print "\t<label for=$var>$text<input type=$type name=$var id=$var min=$date value=$date>\n";
             }
             else if ($type == "check") {
                 print "\t<label for=$var>$text<input type=$type name=$var id=$var>\n";
