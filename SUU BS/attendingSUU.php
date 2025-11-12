5e0be7ee0d62342730d8f0113e9a89e7018c2756
kbb306 <kylerbbelshaw@gmail.com>
2025-11-12 15:58:16 -0700

    Fixed missing labels

--- SUU BS/attendingSUU.php
+++ SUU BS/attendingSUU.php
@@ -72,6 +72,7 @@ function get_form($num=0) {
                 print "\t<label for=$var>$text<input type=$type name=$var id=$var>\n";
             }
             if ($type == "select") {
+                print "<label>$text</label>"
                 $sql = "SELECT * FROM info_for_select_and_radio_input WHERE selector='$var' ORDER BY option";
                 $options = $conn->query($sql);
                 if ($options -> num_rows > 0) {
@@ -87,6 +88,7 @@ function get_form($num=0) {
         }
             if ($type == "radio") {
                 //echo "Radio section.";
+                print "<label>$text</label>
                 $sql = "SELECT * FROM info_for_select_and_radio_input WHERE selector='$var' ORDER BY option";
                 $options = $conn->query($sql);
                 if ($options -> num_rows > 0) {
